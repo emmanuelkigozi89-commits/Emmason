0e94c5947535b9cfbab06b65c058a3a79e41d3fb
emmanuelkigozi89-commits <emmanuelkigozi89@gmail.com>
2026-05-12 13:29:10 +0300

    Add files via upload

---
+++ mine.js
@@ -0,0 +1,6 @@
+const activePage = window.location.pathname;
+const navlinks = document.querySelectorAll('nav a').forEach(link =>{
+    if(link.href.includes(`${activePage}`)){
+        link.classList.add('active');
+    }
+})
